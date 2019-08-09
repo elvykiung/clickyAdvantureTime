0700ae4f5210539ee13118c577962fb8038e84ef
Elvy Kiung <kiungh22@gmail.com>
2019-08-08 23:27:34 -0700

    Updates

---
+++ precache-manifest.d748ae3c81ddaf2efad81806edf7535f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0493b7802cd87a03f9a0da82e392e982",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "3cb741590dbe3f872dac",
+    "url": "/my-app/static/css/main.394010cf.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/my-app/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "3cb741590dbe3f872dac",
+    "url": "/my-app/static/js/main.5d56a187.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
